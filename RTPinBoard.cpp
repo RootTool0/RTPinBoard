994809c59d7cf729c7a51a73aac8fb7783e64818
Root Tool <137654842+RootTool0@users.noreply.github.com>
2023-06-30 22:21:03 +0300

    RTPinBoard.cpp V1.1

--- RTPinBoard.cpp
+++ RTPinBoard.cpp
@@ -1,38 +1,93 @@
-﻿#include <RTPinBoard.h>
+/*
+ * RTPinBoard
+ * Версия: 1.1
+ * Создал: RootTool
+ * https://www.youtube.com/@roottool0
+ *
+ * Видео
+ * https://www.youtube.com/watch?v=Pe57_49BhEs
+ */
+
+#include "RTPinBoard.h"
 
 RTPinBoard::RTPinBoard(byte pin, byte range,int sw1,int sw2,int sw3,int sw4,int sw5) {
-  _pin = pin;
-  _sw1 = sw1;
-  _sw2 = sw2;
-  _sw3 = sw3;
-  _sw4 = sw4;
-  _sw5 = sw5;
-  _range = range;
+	_pin = pin;
+	_value[0] = sw1;
+	_value[1] = sw2;
+	_value[2] = sw3;
+	_value[3] = sw4;
+	_value[4] = sw5;
+	_range = range;
 }
 
-void RTPinBoard::setSW1(int val) {_sw1 = val;}
-void RTPinBoard::setSW2(int val) {_sw2 = val;}
-void RTPinBoard::setSW3(int val) {_sw3 = val;}
-void RTPinBoard::setSW4(int val) {_sw4 = val;}
-void RTPinBoard::setSW5(int val) {_sw5 = val;}
+void RTPinBoard::setValue(byte button, int val) {
+	switch(button){
+		case 1: _value[0] = val; break;
+		case 2: _value[1] = val; break;
+		case 3: _value[2] = val; break;
+		case 4: _value[3] = val; break;
+		case 5: _value[4] = val; break;
+	}
+}
 
 void RTPinBoard::setRange(byte val) {_range = val;}
 void RTPinBoard::setPin(byte val) {_pin = val;}
+int RTPinBoard::readPin() {return _read;}
 
-boolean RTPinBoard::getSW1() {return NumClicked == 1 ? true : false;}
-boolean RTPinBoard::getSW2() {return NumClicked == 2 ? true : false;}
-boolean RTPinBoard::getSW3() {return NumClicked == 3 ? true : false;}
-boolean RTPinBoard::getSW4() {return NumClicked == 4 ? true : false;}
-boolean RTPinBoard::getSW5() {return NumClicked == 5 ? true : false;}
-
-int RTPinBoard::readPin() {return analogRead(_pin);}
+boolean RTPinBoard::isClick(byte button){return _click[button-1];}
+boolean RTPinBoard::isRelease(byte button){return _release[button-1];}
+boolean RTPinBoard::isHold(byte button){return _hold[button-1];}
 
 void RTPinBoard::update(){
-	int read = analogRead(_pin);
-	if(read >= _sw1 - _range && read <= _sw1 + _range) NumClicked = 1;
-	else if(read >= _sw2 - _range && read <= _sw2 + _range) NumClicked = 2;
-	else if(read >= _sw3 - _range && read <= _sw3 + _range) NumClicked = 3;
-	else if(read >= _sw4 - _range && read <= _sw4 + _range) NumClicked = 4;
-	else if(read >= _sw5 - _range && read <= _sw5 + _range) NumClicked = 5;
-	else NumClicked = 0;
+	_read = analogRead(_pin);
+	if(_read >= _value[0] - _range && _read <= _value[0] + _range){
+	  memset(_btnState, false, 5);
+    _btnState[0] = true;
+  }
+	else if(_read >= _value[1] - _range && _read <= _value[1] + _range){
+	  memset(_btnState, false, 5);
+    _btnState[1] = true;
+  }
+	else if(_read >= _value[2] - _range && _read <= _value[2] + _range){
+	  memset(_btnState, false, 5);
+    _btnState[2] = true;
+  }
+	else if(_read >= _value[3] - _range && _read <= _value[3] + _range){
+	  memset(_btnState, false, 5);
+    _btnState[3] = true;
+  }
+	else if(_read >= _value[4] - _range && _read <= _value[4] + _range)  {
+    memset(_btnState, false, 5);
+    _btnState[4] = true;
+	}
+	else {memset(_btnState, false, 5);}
+	
+	for(int i = 0; i < 5; i++){
+		if (_btnState[i] && !_flag[i] && millis() - btnTimer[i] > 100) {
+      _flag[i] = true;
+      btnTimer[i] = millis();
+      _click[i] = true;
+      _release[i] = false;
+      _hold[i] = false;
+    }
+    if (_btnState[i] && _flag[i] && millis() - btnTimer[i] > 200) {
+      btnTimer[i] = millis();
+      _click[i] = false;
+      _release[i] = false;
+      _hold[i] = true;
+    }
+    if (!_btnState[i] && !_flag[i]) {
+      _click[i] = false;
+      _release[i] = false;
+      _hold[i] = false;
+    }
+    
+    if (!_btnState[i] && _flag[i] && millis() - btnTimer[i] > 100) {
+      _flag[i] = false;
+      btnTimer[i] = millis();
+      _click[i] = false;
+      _release[i] = true;
+      _hold[i] = false;
+    }
+  }
 }
